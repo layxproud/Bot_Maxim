8980dd6452b81dce26fd71e35d9c9b50254c3e11
LayxProud <layxproud@mail.ru>
2022-11-20 07:33:15 +0300

    minor changes

--- commands/warn.py
+++ commands/warn.py
@@ -1,10 +1,10 @@
-def warn(bot, chat_id, fwd_user, user_name):
+def warn(bot, chat_id, fwd_user):
     if bot.isAdmin(chat_id, fwd_user.vk_id):
         bot.messageSender(chat_id, 'Не получится!')
     else:
         fwd_user.warns += 1
         fwd_user.save()
-        bot.messageSender(chat_id, f'@id{fwd_user.vk_id}({user_name}), вам выдано предупреждение!\nВсего предупреждений: {fwd_user.warns}/3')
+        bot.messageSender(chat_id, f'@id{fwd_user.vk_id}({fwd_user.name}), вам выдано предупреждение!\nВсего предупреждений: {fwd_user.warns}/3')
         
         if fwd_user.warns >= 3:
             bot.vk_session.method('messages.removeChatUser', {
@@ -14,10 +14,10 @@ def warn(bot, chat_id, fwd_user, user_name):
             fwd_user.warns = 0
             fwd_user.save()
             
-def unwarn(bot, chat_id, fwd_user, user_name):
+def unwarn(bot, chat_id, fwd_user):
     if fwd_user.warns > 0:
         fwd_user.warns -= 1
         fwd_user.save()
-        bot.messageSender(chat_id, f"С пользователя @id{fwd_user.vk_id}({user_name}) снято 1 предупреждение.")   
+        bot.messageSender(chat_id, f"С пользователя @id{fwd_user.vk_id}({fwd_user.name}) снято 1 предупреждение.")   
     else:
-        bot.messageSender(chat_id, f"У пользователя @id{fwd_user.vk_id}({user_name}) нет предупреждений.")  
+        bot.messageSender(chat_id, f"У пользователя @id{fwd_user.vk_id}({fwd_user.name}) нет предупреждений.")  
